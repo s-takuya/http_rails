7445b46403e0c1cca3002926c9c248fdd0f86c01
s-takuya <takuyashiratake0622@icloud.com>
2016-08-03 19:08:54 +0900

    Remove csrf validation

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+  protect_from_forgery with: :null_session
 
   # GET /tweets
   # GET /tweets.json
